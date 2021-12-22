44a43eb2ec6a3008e54004fca313a7474faabe97
gtsamsapo <gtsamsapo@sapo.pt>
2021-12-22 13:57:22 +0000

    another test

--- app/models/todo.js
+++ app/models/todo.js
@@ -5,6 +5,19 @@ var i;
 for (i = 0; i < s.length; i = i + 1) {
   console.log(s.charAt(i));
 }
+
+
+var n = 123;
+var s = '123';
+
+if (n == s) {
+  alert('The variables were equal');
+}
+
+if (n === s) {
+  alert('The variables were identical');
+}
+
 module.exports = mongoose.model('Expense', {
     data: {
         type: Object,
