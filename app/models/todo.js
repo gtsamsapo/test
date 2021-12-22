4371e683357c62eb350a5051b1a272f3f38673e1
gtsamsapo <gtsamsapo@sapo.pt>
2021-12-22 13:56:18 +0000

    another test

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,5 +1,10 @@
 var mongoose = require('mongoose');
 var i = 0;
+var s = 'mystring';
+var i;
+for (i = 0; i < s.length; i = i + 1) {
+  console.log(s.charAt(i));
+}
 module.exports = mongoose.model('Expense', {
     data: {
         type: Object,
